a4c95dc2d12f49d560605241e862d36ccc1c4204
aero <chahkang@gmail.com>
2011-09-23 10:16:48 +0900

    change the location of is_a_following_b method.

--- lib/Tweetalyst/API/Follow.pm
+++ lib/Tweetalyst/API/Follow.pm
@@ -4,10 +4,11 @@ use namespace::autoclean;
 
 with 'Tweetalyst::API::WithDBIC';
 
-__PACKAGE__->meta->make_immutable();
-
 sub is_a_following_b {
     my ($self, $a, $b) = @_;
     return $self->resultset->search( { source => $a, destination => $b } )->count;
 }
+
+__PACKAGE__->meta->make_immutable();
+
 1;
